8abb41eb462c95a575744cd8a8b8a5cfcac066af
Altaha <altahabinsyafriadi@gmail.com>
2022-01-03 17:09:23 +0700

    update bug on entities

--- driver/entities.go
+++ driver/entities.go
@@ -3,6 +3,7 @@ package driver
 import(
 	UserModel "vac/features/user/data"
 	AdminModel "vac/features/admin/data"
+	VacModel "vac/features/vac/data"
 )
 type Entity struct{
 	Model interface{}
@@ -12,5 +13,7 @@ func registerEntities() []Entity{
 	return []Entity{
 		{UserModel.User{}},
 		{AdminModel.Admin{}},
+		{VacModel.Vac{}},
+		{VacModel.Session{}},
 	}
 }
