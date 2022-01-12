7701e87ed689450b3799208cf74ac3ca59bdd5bd
Altaha <altahabinsyafriadi@gmail.com>
2022-01-12 17:39:51 +0700

    add git ignore

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"vac/driver"
 	"vac/routes"
+
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	driver.InitDB()
-	e:=routes.New()
+	e := routes.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+	}))
 	e.Start("127.0.0.1:8000")
-	
+
 }
